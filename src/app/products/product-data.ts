43bc0cd4cb2a0be8b0f80c4c7ce4c0b79178eaad
Djan K. Blom <djan@live.dk>
2019-10-19 17:21:34 +0200

    products updated

--- src/app/products/product-data.ts
+++ src/app/products/product-data.ts
@@ -8,35 +8,35 @@ export class ProductData implements InMemoryDbService {
         const products: Product[] = [
             {
                 'id': 1,
-                'productName': 'Leaf Rake',
+                'productName': 'Lance of Longinus',
                 'productCode': 'GDN-0011',
                 'description': 'Leaf rake with 48-inch wooden handle',
                 'starRating': 3.2
             },
             {
                 'id': 2,
-                'productName': 'Garden Cart',
+                'productName': 'The Horn of Gabriel',
                 'productCode': 'GDN-0023',
                 'description': '15 gallon capacity rolling garden cart',
                 'starRating': 4.2
             },
             {
                 'id': 5,
-                'productName': 'Hammer',
+                'productName': 'Hammer of Death',
                 'productCode': 'TBX-0048',
                 'description': 'Curved claw steel hammer',
                 'starRating': 4.8
             },
             {
                 'id': 8,
-                'productName': 'Saw',
+                'productName': 'Spear of Destiny',
                 'productCode': 'TBX-0022',
                 'description': '15-inch steel blade hand saw',
                 'starRating': 3.7
             },
             {
                 'id': 10,
-                'productName': 'Video Game Controller',
+                'productName': 'Bane of Dionysus',
                 'productCode': 'GMG-0042',
                 'description': 'Standard two-button video game controller',
                 'starRating': 4.6
